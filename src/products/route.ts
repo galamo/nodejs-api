91bcb09c6489a46f97b19403cf6c7991c758e346
zeev mindali <mindalizeev@gmail.com>
2023-06-18 21:19:34 +0300

    zod

--- src/products/route.ts
+++ src/products/route.ts
@@ -1,7 +1,17 @@
 
 import express from "express"
 import products from "./data"
+import zod from "zod"
 const router = express.Router();
+
+const productBody = zod.object({
+    id: zod.number(),
+    title: zod.string().max(20).optional(),
+    images: zod.array(zod.string()),
+    rating: zod.number().max(5),
+    category: zod.enum(["dairy", "drinks", "food", "fruits",])
+})
+
 
 
 router.get("/", function (req, res, next) {
@@ -11,6 +21,46 @@ router.get("/", function (req, res, next) {
 
 router.get("/:id", function (req, res, next) {
     return res.json(products) // filter by id
+})
+
+router.post("/new", function (req, res, next) {
+
+    try {
+        productBody.parse(req.body)
+
+        const { id,
+            title,
+            description,
+            price,
+            discountPercentage,
+            rating,
+            stock,
+            brand,
+            category,
+            thumbnail,
+            images } = req.body;
+        products.push({
+            id,
+            title,
+            description,
+            price,
+            discountPercentage,
+            rating,
+            stock,
+            brand,
+            category,
+            thumbnail,
+            images
+        })
+        return res.send("product added")
+    } catch (error) {
+        const { errors } = error
+        console.log(errors[0].path[0] + "=> " + errors[0].message)
+        res.status(400).json({ error: errors[0].path[0] + "=> " + errors[0].message })
+    }
+
+
+
 })
 
 export default router;
